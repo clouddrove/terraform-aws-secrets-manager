73a8a9820d5bd5071e3bf061935d8a06338ea45b
yadavprakash <yadavprakash.it@gmail.com>
2021-07-02 20:01:11 +0530

    update github-action,module tags and update in 0.15

--- _examples/plaintext/main.tf
+++ _examples/plaintext/main.tf
@@ -24,4 +24,3 @@ module "secrets_manager" {
     }
   ]
 }
-
--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "environment" {
 
 variable "repository" {
   type        = string
-  default     = "https://registry.terraform.io/modules/clouddrove/secrets-manager/aws/"
+  default     = "https://github.com/clouddrove/terraform-aws-secrets-manager"
   description = "Terraform current module repo"
 }
 
